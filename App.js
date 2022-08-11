8da185be9a080b01c61e0be4b8659e53016b6bd8
Raj Prajapat <rajprajapat@192.168.1.4>
2022-08-12 00:45:57 +0530

    Setup 3 Environment dev, statging and live

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Config from 'react-native-config';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Sample + {Config.NODE_ENV}</Text>
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
